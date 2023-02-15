96087245cf1e60e9803918f8e58d76595a51735e
khaleel <shaikkhaleelulla540@gmail.com>
2023-02-15 10:45:48 +0530

    commit

--- routes.js
+++ routes.js
@@ -1,22 +1,39 @@
 const express = require('express');
 const twilio = require('twilio')
-const ContactsRouter = express.Router();
+const router = express.Router();
 
 const list = require('./contactDetails.json')
 const OtpModel = require('./Models/otpModel')
 
-
-ContactsRouter.get('/', (req, res) => {
-    res.render('list', { list })
+router.get('/', async (req, res) => {
+    res.render('index')
 })
 
-ContactsRouter.post('/sendmessage', async (req, res) => {
+router.get('/contacts', (req, res) => {
+    res.render('list', { list })
+})
+router.get('/contacts/:firstName', (req, res) => {
+    const details = list.find(ele =>
+        req.params.firstName == ele.firstName)
+    res.render('contactDetails', { details, port:process.env.PORT })
+})
+
+router.get('/contacts/sendmessage/:firstName', (req, res) => {
+    const details = list.find(ele =>
+        req.params.firstName == ele.firstName)
+    const otp = Math.round(Math.random() * 900000 + 100000)
+    const message = `Hi. Your OTP is: ${otp}`
+    res.render('sendMessage', { details, message, flashMessage: req.flash('flashMessage') })
+})
+
+
+router.post('/contacts/sendmessage', async (req, res) => {
     try {
         const details = list.find(ele => req.body.firstName == ele.firstName)
         let accountSid = process.env.TWILIO_ACCOUNT_SID;
         let authToken = process.env.TWILIO_AUTH_TOKEN;
         const client = new twilio(accountSid, authToken);
-        const response = await client.messages.create({ body: req.body.message, from: process.env.PHONE_NUMBER, to: "+91" + details.phoneNumber })
+        const response = await client.messages.create({ body: req.body.message, from: process.env.PHONE_NUMBER, to: "+91"+details.phoneNumber })
         const result = await OtpModel.create({ name: req.body.firstName, sms: req.body.message })
         req.flash('flashMessage', 'OTP sent successfull')
         res.redirect('/contacts/sendmessage/' + details.firstName)
@@ -26,18 +43,9 @@ ContactsRouter.post('/sendmessage', async (req, res) => {
     }
 });
 
-ContactsRouter.get('/sendmessage/:firstName', (req, res) => {
-    const details = list.find(ele =>
-        req.params.firstName == ele.firstName)
-    const otp = Math.round(Math.random() * 900000 + 100000)
-    const message = `Hi. Your OTP is: ${otp}`
-    res.render('sendMessage', { details, message, flashMessage: req.flash('flashMessage') })
+router.get('/messages', async (req, res) => {
+    const messageList = await OtpModel.find().sort({ createdAt: -1 })
+    res.render('messages', { messageList })
 })
 
-ContactsRouter.get('/:firstName', (req, res) => {
-    const details = list.find(ele =>
-        req.params.firstName == ele.firstName)
-    res.render('contactDetails', { details })
-})
-
-module.exports = ContactsRouter
+module.exports = router
